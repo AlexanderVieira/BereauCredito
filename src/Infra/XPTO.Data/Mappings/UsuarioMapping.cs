4e19d28f12a90368f09da6fed0eeb7825a68e74c
Alexander Silva <alexander.silva@al.infnet.edu.br>
2022-12-13 23:50:48 -0300

    feat[bereau-credito::mapping]: mapeamento com fluentvalidation - Implementacao classes de configuracao para mapear os relacionamentos cliente e usuario.

---
+++ src/Infra/XPTO.Data/Mappings/UsuarioMapping.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using XPTO.Domain.Entities;
+
+namespace XPTO.Data.Mappings
+{
+    public class UsuarioMapping : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.HasKey(u => u.Id);
+
+            builder.Property(u => u.Login)
+                .IsRequired()
+                .HasColumnName("Login")
+                .HasColumnType("varchar(100)");
+
+            builder.OwnsOne(u => u.Senha, tf =>
+            {
+                tf.Property(u => u.Valor)
+                .IsRequired()
+                .HasColumnName("Senha")
+                .HasColumnType($"varchar(8)");
+            });
+
+            builder.HasMany(u => u.Operacoes)
+                .WithMany(o => o.Usuarios);
+
+            builder.ToTable("Usuarios");
+        }
+    }
+}
